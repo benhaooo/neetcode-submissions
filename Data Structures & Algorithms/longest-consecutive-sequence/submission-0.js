3a6dd8657be64c18660016b43e6d4055541240ff
benhaooo <84887579+benhaooo@users.noreply.github.com>
2026-05-06 19:34:59 +0800

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let maxLen = 1;
+        let curLen = 1;
+
+        nums = nums.sort((a, b) => a - b);
+        for (let i = 0; i < nums.length - 1; i++) {
+            if (nums[i] === nums[i + 1]) continue;
+            if (nums[i] + 1 === nums[i + 1]) {
+                curLen++;
+            } else {
+                curLen = 1;
+            }
+            maxLen = Math.max(maxLen, curLen);
+        }
+
+        return maxLen;
+    }
+}
