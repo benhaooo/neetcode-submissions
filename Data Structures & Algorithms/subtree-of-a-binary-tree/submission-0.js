e85b2e866b2e9e7e11677b3bafe01c571c72eafe
benhaooo <84887579+benhaooo@users.noreply.github.com>
2026-08-04 18:43:17 +0800

    Add: subtree-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} subRoot
+     * @return {boolean}
+     */
+    isSubtree(root, subRoot) {
+        if (root === null && subRoot === null) return true
+        if (root === null) return false
+        if (this.isSametree(root, subRoot)) return true
+        return this.isSubtree(root.left, subRoot) || this.isSubtree(root.right, subRoot)
+    }
+
+    isSametree(p, q) {
+        if (p === null && q === null) return true
+        if (p === null || q === null) return false
+        if (p.val !== q.val) return false
+        return this.isSametree(p.left, q.left) && this.isSametree(p.right, q.right)
+    }
+}
+
