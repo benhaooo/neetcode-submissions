8e3323ecc776f78383e1bd2c5be97faee717ce20
benhaooo <84887579+benhaooo@users.noreply.github.com>
2026-08-18 18:17:54 +0800

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.js
@@ -0,0 +1,79 @@
+
+class Heap {
+    constructor(compare = (a, b) => { a - b }) {
+        this.data = []
+        this.compare = compare
+    }
+    get size() { return this.data.length }
+    peek() {
+        if (!this.size) return
+        return this.data[0]
+    }
+    pop() {
+        if (!this.size) return
+        const top = this.data[0]
+
+        const last = this.data.pop()
+
+        if (this.size) {
+            this.data[0] = last
+            this.#down(0)
+        }
+        return top
+    }
+    push(value) {
+        this.data.push(value)
+        this.#up(this.size - 1)
+    }
+
+
+    #up(i) {
+        let cur = i
+        while (true) {
+            const p = Math.floor((cur - 1) / 2)
+            if (p < 0) break
+            if (this.compare(this.data[p], this.data[cur]) < 0) break
+            this.#swap(cur, p)
+            cur = p
+        }
+    }
+    #down(i) {
+        let cur = i
+        while (cur < this.size - 1) {
+            let child = 2 * cur + 1
+            if (child > this.size - 1) break
+            const right = 2 * cur + 2
+            if (right <= this.size - 1 && this.compare(this.data[child], this.data[right]) >= 0) {
+                child = right
+            }
+            if (this.compare(this.data[cur], this.data[child]) < 0) break
+            this.#swap(cur, child)
+            cur = child
+        }
+    }
+    #swap(i, j) {
+        [this.data[i], this.data[j]] = [this.data[j], this.data[i]]
+    }
+}
+
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones) {
+        if (!stones.length) return
+        if (stones.length < 2) return stones[0]
+        const heap = new Heap((a, b) => b - a)
+        for (const stone of stones) {
+            heap.push(stone)
+        }
+        while (heap.size >= 2) {
+            const x = heap.pop()
+            const y = heap.pop()
+            const z = Math.abs(x - y)
+            heap.push(z)
+        }
+        return heap.peek()
+    }
+}
