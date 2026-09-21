85d180ce343928818a2dd2f624a42ce28a9b168b
benhaooo <84887579+benhaooo@users.noreply.github.com>
2026-09-21 18:04:00 +0800

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost) {
+        const n = cost.length
+        const dp = Array.from({ length: n + 1 }, () => 0)
+        dp[0] = 0
+        dp[1] = 0
+        for (let i = 2; i < dp.length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        }
+
+        return dp[n]
+    }
+}
