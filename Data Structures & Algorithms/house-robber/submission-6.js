c0960a9c9c372e36bdfa1215f6efead93beba2ac
benhaooo <84887579+benhaooo@users.noreply.github.com>
2026-09-23 18:58:16 +0800

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+
+        const n = nums.length
+        if (n === 1) return nums[0]
+        let prev1 = Math.max(nums[0], nums[1])
+        let prev2 = nums[0]
+        for (let i = 2; i < n; i++) {
+            const curr = Math.max(prev1, prev2 + nums[i])
+            prev2 = prev1
+            prev1 = curr
+        }
+
+        return prev1
+    }
+}
